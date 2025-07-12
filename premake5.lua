f1cf8d897d8aa18ca0a192dbc5785628dfc8dccf
odysseia06 <floydianayhan@gmail.com>
2025-07-12 23:41:37 +0300

    Add all files and remove unnecessary ones manually

--- premake5.lua
+++ premake5.lua
@@ -4,57 +4,26 @@ project "GLFW"
 	warnings "off"
 	staticruntime "on"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-
+	objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
 	files {
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/platform.c",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c",
-		"src/null_init.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-		"src/null_joystick.c"
+		"include/GLFW/*.h",
+        "src/*.c"
 	}
 	filter "system:windows"
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
+		defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+		removefiles {
+        "src/x11_*.c",
+        "src/posix_*.c",
+        "src/cocoa_*.c",
+        "src/null_*.c"
 		}
 	filter "system:linux"
-		files {
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/x11_xkb.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+		defines { "_GLFW_X11" }
+		removefiles {
+        "src/win32_*.c",
+        "src/cocoa_*.c"
 		}
-		defines { "_GLFW_X11" }	
 	filter "configurations:Debug"
 		symbols "on"
 	filter "configurations:Release"
